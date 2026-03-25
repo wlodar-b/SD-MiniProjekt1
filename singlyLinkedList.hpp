468782ea09e26bbe2b137308083bf07819fcf1ce
Bartek <wlodarcykb16@gmail.com>
2026-03-25 12:35:15 +0100

    fix: komentarze do pliku nagłówkowego listy jednokierunkowej

--- singlyLinkedList.hpp
+++ singlyLinkedList.hpp
@@ -12,34 +12,32 @@ struct Node {
 
 // Klasa reprezentująca listę jednokierunkową
 class SinglyLinkedList {
+
     private:
-        Node* head;     // Wskaźnik na pierwszy element listy
-        Node* tail;     // Wskaźnik na ostatni elemnt listy
+        Node* head;     // pierwszy element listy
+        Node* tail;     // ostatni elemnt listy
         int size;       // Liczba przechowywanych elementów
 
     public:
-        // Konstruktor - inicjalizujący pustą listę
-        SinglyLinkedList();
-
-        // Destruktor - musi zwolnić pamięć wszystkich węzłów 
-        ~SinglyLinkedList();
+        SinglyLinkedList();         // konstruktor 
+        ~SinglyLinkedList();        // destruktor
 
         // Operacje dodawania elementu
-        void addStart(int value);   // Dodawanie na początek - O(1)
-        void addEnd(int value);     // Dodawanie na koniec - O(1) dzięki wskaźnikowi
-        void addAt(int index, int value); // Dodawanie miejscu w losowe miejsce - O(n)
+        void addStart(int value);   // Dodawanie na początek
+        void addEnd(int value);     // Dodawanie na koniec 
+        void addAt(int index, int value); // Dodawanie miejscu w losowe miejsce 
 
         // Operacja usuwania elementu
-        void removeStart();         // Usuwanie z początku - O(1)
-        void removeEnd();           // Usuwanie z końca - O(n) (trzeba znaleźć przedostatni element)
-        void removeAt(int index);   // Usuwanie z losowego miejsca - O(n)
+        void removeStart();         // Usuwanie z początku 
+        void removeEnd();           // Usuwanie z końca 
+        void removeAt(int index);   // Usuwanie z losowego miejsca 
 
         // Operacja wyszukiwania
-        int find(int value);        // Zwraca indeks elementu lub -1)
+        int find(int value);        // Zwraca indeks elementu lub (-1)
 
         // Metody pomocnicze do menu i badań
         void display();             // Wyświetlenie zawartości listy
-        void clear();               // Czyszczenie całej listy (usuwanie wszystkich węzłów)
+        void clear();               // Czyszczenie całej listy 
         int getSize();              // Zwraca aktualny rozmiar n
 };
 
